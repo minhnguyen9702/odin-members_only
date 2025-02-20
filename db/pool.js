31822b1d1e41f181cfddc7961e80e4b422343525
minhnguyen9702 <minhnguyen9702@gmail.com>
2025-02-20 20:45:01 +0700

    connect app to database

---
+++ db/pool.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+module.exports = new Pool({
+  connectionString: connectionString,
+});
